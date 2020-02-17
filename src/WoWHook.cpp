f90f7115f88b1bf2f2175198de44adc6d8a3b746
Ellie <ellie1337@gmail.com>
2020-02-16 21:34:51 -0800

    Basic do-nothing dll

---
+++ src/WoWHook.cpp
@@ -0,0 +1,21 @@
+﻿#include "preCompiled.h"
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		break;
+
+	case DLL_PROCESS_DETACH:
+		break;
+
+	case DLL_THREAD_ATTACH:
+		break;
+
+	case DLL_THREAD_DETACH:
+		break;
+	}
+
+	return TRUE;
+}
---
+++ src/preCompiled.cpp
@@ -0,0 +1,1 @@
+#include "preCompiled.h"
