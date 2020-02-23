2b17c22c4a88219d7d5db693d9a131c8287ed34b
Ellie <ellie1337@gmail.com>
2020-02-22 20:09:21 -0800

    Forgot virtual

--- src/WoW/CPlayer.h
+++ src/WoW/CPlayer.h
@@ -3,90 +3,90 @@
 class CPlayer
 {
 public:
-	void unk006E78D0(); // 006E78D0 - ~Player() ?
-	void unk006E6020(); // 006E6020 - ~Player() ?
-	void unk006E6150(); // 006E6150
-	void unk006E1D70(); // 006E1D70
-	void unk00632050(); // 00632050
-	void unk007370D0(); // 007370D0
-	void unk00714C40(); // 00714C40
-	void unk0073E410(); // 0073E410
-	void unk0071FEF0(); // 0071FEF0
-	void unk006E6FD0(); // 006E6FD0
-	void unk006E6FD0(); // 006E6FD0
-	void unk006E6EF0(int* unk); // 006E6EF0 - Uses player position
-	void unk006E6F10(); // 006E6F10
-	void unk006E6F40(); // 006E6F40
-	void unk006E6F60(); // 006E6F60
-	void unk004D5F00(); // 004D5F00
-	void unk006E6F70(); // 006E6F70
-	void unk004D5F20(); // 004D5F20
-	void unk00744380(); // 00744380
-	void unk007192C0(); // 007192C0 - check some kind of flag
-	void unk00729F40(); // 00729F40
-	void unk007192E0(); // 007192E0
-	void unk00719310(); // 00719310
-	void unk007193C0(); // 007193C0
-	void unk00717B20(); // 00717B20 - Spells\ErrorCode.mdx
-	void unk00724DF0(); // 00724DF0
-	void unk00724E20(); // 00724E20
-	void unk00725980(); // 00725980
-	void unk00725BF0(); // 00725BF0
-	void unk0072B7E0(); // 0072B7E0
-	void unk00718AC0(); // 00718AC0
-	void unk0071C0E0(); // 0071C0E0
-	void unk0073E840(int unk); // 0073E840 - something about mounts?
-	void unk0071AC30(); // 0071AC30
-	void unk006D5EB0(); // 006D5EB0
-	void unk0071FD80(float unk); // 0071FD80
-	void unk006E0840(bool unk, bool* unk2, bool* unk3); // 006E0840 - something to do with commentator mode
-	void unk007156A0(); // 007156A0
-	void unk0073AD60(); // 0073AD60
-	bool returntrue(); // 008C8DE0
-	void unk004899F0(); // 004899F0
-	bool isShowingXPBar(); // 0071A390
-	void unk006E6EC0(); // 006E6EC0
-	void unk008E5250(); // 008E5250
-	void unk00731260(); // 00731260
-	bool canGainXP(); // 006E6ED0
-	void unk0073C1D0(); // 0073C1D0
-	void unk00720F80(); // 00720F80
-	void unk00722760(); // 00722760
-	void unk00722B50(); // 00722B50
-	void unk00715560(); // 00715560
-	void unk006E6FA0(); // 006E6FA0
-	void unk00729C70(); // 00729C70
-	int* getSomething(); // 006E6F80 - returns [plr+90C] or [[plr+90C]+0B4]
-	void unk006E6EE0(); // 006E6EE0
-	void unk004899F0(); // 004899F0
-	void unk0074B830(); // 0074B830
-	void unk00716650(); // 00716650
-	void unk00715B50(); // 00715B50
-	bool returnTrue(); // 008A1420
-	void unk00959DE0(); // 00959DE0
-	void unk0074B810(); // 0074B810
-	void unk006F1700(); // 006F1700
-	void unk0073B140(); // 0073B140
-	void unk0073A6C0(); // 0073A6C0
-	void unk0072AF60(); // 0072AF60
-	const char* ChatTag_AFK(); // 006DC8D0
-	void ChatTag_DND(char* output, int length); // 006DC940
-	void ChatTag_GM(char* output, int length); // 006DC980
-	void ChatTag_Dev(char* output, int length); // 006DC9D0
-	void unk007631A0(int unk, int* unk2, float unk3); // 007631A0
-	void Sound_EnableArmorFoleySound(); // 007633F0
-	void unk007634F0(); // 007634F0
-	void unk006D5BE0(); // 006D5BE0
-	bool isGhost(); // 006DAC10
-	void unk006E04D0(); // 006E04D0
-	void unk006DC7E0(); // 006DC7E0
-	void unk006E05D0(int counter); // 006E05D0
-	void unk006D64E0(); // 006D64E0
-	void unk006DC330(); // 006DC330
-	void unk006DE6D0(int unk1, int unk2); // 006DE6D0
-	void unk006E0C30(); // 006E0C30
-	void unk006D68D0(); // 006D68D0
-	void unk006E6FC0(); // 006E6FC0
+	virtual void unk006E78D0(); // 006E78D0 - ~Player() ?
+	virtual void unk006E6020(); // 006E6020 - ~Player() ?
+	virtual void unk006E6150(); // 006E6150
+	virtual void unk006E1D70(); // 006E1D70
+	virtual void unk00632050(); // 00632050
+	virtual void unk007370D0(); // 007370D0
+	virtual void unk00714C40(); // 00714C40
+	virtual void unk0073E410(); // 0073E410
+	virtual void unk0071FEF0(); // 0071FEF0
+	virtual void unk006E6FD0(); // 006E6FD0
+	virtual void unk006E6FD0(); // 006E6FD0
+	virtual void unk006E6EF0(int* unk); // 006E6EF0 - Uses player position
+	virtual void unk006E6F10(); // 006E6F10
+	virtual void unk006E6F40(); // 006E6F40
+	virtual void unk006E6F60(); // 006E6F60
+	virtual void unk004D5F00(); // 004D5F00
+	virtual void unk006E6F70(); // 006E6F70
+	virtual void unk004D5F20(); // 004D5F20
+	virtual void unk00744380(); // 00744380
+	virtual void unk007192C0(); // 007192C0 - check some kind of flag
+	virtual void unk00729F40(); // 00729F40
+	virtual void unk007192E0(); // 007192E0
+	virtual void unk00719310(); // 00719310
+	virtual void unk007193C0(); // 007193C0
+	virtual void unk00717B20(); // 00717B20 - Spells\ErrorCode.mdx
+	virtual void unk00724DF0(); // 00724DF0
+	virtual void unk00724E20(); // 00724E20
+	virtual void unk00725980(); // 00725980
+	virtual void unk00725BF0(); // 00725BF0
+	virtual void unk0072B7E0(); // 0072B7E0
+	virtual void unk00718AC0(); // 00718AC0
+	virtual void unk0071C0E0(); // 0071C0E0
+	virtual void unk0073E840(int unk); // 0073E840 - something about mounts?
+	virtual void unk0071AC30(); // 0071AC30
+	virtual void unk006D5EB0(); // 006D5EB0
+	virtual void unk0071FD80(float unk); // 0071FD80
+	virtual void unk006E0840(bool unk, bool* unk2, bool* unk3); // 006E0840 - something to do with commentator mode
+	virtual void unk007156A0(); // 007156A0
+	virtual void unk0073AD60(); // 0073AD60
+	virtual bool returntrue(); // 008C8DE0
+	virtual void unk004899F0(); // 004899F0
+	virtual bool isShowingXPBar(); // 0071A390
+	virtual void unk006E6EC0(); // 006E6EC0
+	virtual void unk008E5250(); // 008E5250
+	virtual void unk00731260(); // 00731260
+	virtual bool canGainXP(); // 006E6ED0
+	virtual void unk0073C1D0(); // 0073C1D0
+	virtual void unk00720F80(); // 00720F80
+	virtual void unk00722760(); // 00722760
+	virtual void unk00722B50(); // 00722B50
+	virtual void unk00715560(); // 00715560
+	virtual void unk006E6FA0(); // 006E6FA0
+	virtual void unk00729C70(); // 00729C70
+	virtual int* getSomething(); // 006E6F80 - returns [plr+90C] or [[plr+90C]+0B4]
+	virtual void unk006E6EE0(); // 006E6EE0
+	virtual void unk004899F0(); // 004899F0
+	virtual void unk0074B830(); // 0074B830
+	virtual void unk00716650(); // 00716650
+	virtual void unk00715B50(); // 00715B50
+	virtual bool returnTrue(); // 008A1420
+	virtual void unk00959DE0(); // 00959DE0
+	virtual void unk0074B810(); // 0074B810
+	virtual void unk006F1700(); // 006F1700
+	virtual void unk0073B140(); // 0073B140
+	virtual void unk0073A6C0(); // 0073A6C0
+	virtual void unk0072AF60(); // 0072AF60
+	virtual const char* ChatTag_AFK(); // 006DC8D0
+	virtual void ChatTag_DND(char* output, int length); // 006DC940
+	virtual void ChatTag_GM(char* output, int length); // 006DC980
+	virtual void ChatTag_Dev(char* output, int length); // 006DC9D0
+	virtual void unk007631A0(int unk, int* unk2, float unk3); // 007631A0
+	virtual void Sound_EnableArmorFoleySound(); // 007633F0
+	virtual void unk007634F0(); // 007634F0
+	virtual void unk006D5BE0(); // 006D5BE0
+	virtual bool isGhost(); // 006DAC10
+	virtual void unk006E04D0(); // 006E04D0
+	virtual void unk006DC7E0(); // 006DC7E0
+	virtual void unk006E05D0(int counter); // 006E05D0
+	virtual void unk006D64E0(); // 006D64E0
+	virtual void unk006DC330(); // 006DC330
+	virtual void unk006DE6D0(int unk1, int unk2); // 006DE6D0
+	virtual void unk006E0C30(); // 006E0C30
+	virtual void unk006D68D0(); // 006D68D0
+	virtual void unk006E6FC0(); // 006E6FC0
 	
 
 };
